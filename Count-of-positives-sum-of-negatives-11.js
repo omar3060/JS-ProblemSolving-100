bbfc55cc68c2b026747036ee58034095a9e97479
omar mahmoud <omarmahmoudcs@gmail.com>
2025-03-15 01:19:28 +0200

    Add another solution for  problem 11

--- Count-of-positives-sum-of-negatives-11.js
+++ Count-of-positives-sum-of-negatives-11.js
@@ -1,16 +1,25 @@
-function countPositiveSumNegative(arr) {
-  if (!arr || arr.length === 0) return []
+// function countPositiveSumNegative(arr) {
+//   if (!arr || arr.length === 0) return []
 
-  let count = 0
-  let sum = 0
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0){
-      count++
-    } else {
-      sum += arr[i]
-    }
-  }
-  return [count, sum]
+//   let count = 0
+//   let sum = 0
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0){
+//       count++
+//     } else {
+//       sum += arr[i]
+//     }
+//   }
+//   return [count, sum]
+// }
+
+// console.log(countPositiveSumNegative([1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]))
+
+
+function countPositiveSumNegative(arr) {
+  let positiveArray = arr.filter(item => item > 0).length
+  let sumNegative = arr.filter(item => item < 0).reduce((acc, curr) => acc + curr, 0)
+  return [positiveArray, sumNegative]
 }
 
 console.log(countPositiveSumNegative([1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15]))
